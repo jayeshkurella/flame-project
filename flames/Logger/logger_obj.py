5e5d2b2f09f426e8a80f12bc6b3949e86b134f07
jayeshkurella <138563650+jayeshkurella@users.noreply.github.com>
2024-08-01 16:29:24 +0530

    Update logger_obj.py

--- flames/Logger/logger_obj.py
+++ flames/Logger/logger_obj.py
@@ -7,41 +7,51 @@ date = datetime.date.today()
 
 class Logger:
     def __init__(self):
-        self.create_logs_directory()
-        self.logger = self._setup_logger('SMART_logger', f"Logger/user_log_files/log_{date}.log", level=logging.INFO)
-        self.dev_logger = self._setup_logger('SMARTdev_logger', f"Logger/dev_log_files/devlog_{date}.log", level=logging.DEBUG)
+        try:
+            self.create_logs_directory()
+            log_file = os.path.join("Logger/user_log_files", f"log_{date}.log")
+            dev_log_file = os.path.join("Logger/dev_log_files", f"devlog_{date}.log")
+            self.logger = self._setup_logger('SMART_logger', log_file, level=logging.INFO)
+            self.dev_logger = self._setup_logger('SMARTdev_logger', dev_log_file, level=logging.DEBUG)
+        except Exception as e:
+            print(f"Failed to initialize Logger: {e}", file=sys.stderr)
+            sys.exit(1)
 
     @staticmethod
     def create_logs_directory():
         this_folder = os.path.dirname(__file__)
-
         user_log_folder = os.path.join(this_folder, "Logger/user_log_files")
         dev_log_folder = os.path.join(this_folder, "Logger/dev_log_files")
 
-        if not os.path.exists(user_log_folder):
-            print(f"Creating log folder: {user_log_folder}")
-            os.makedirs(user_log_folder)
-
-        if not os.path.exists(dev_log_folder):
-            print(f"Creating log folder: {dev_log_folder}")
-            os.makedirs(dev_log_folder)
+        for folder in [user_log_folder, dev_log_folder]:
+            try:
+                if not os.path.exists(folder):
+                    print(f"Creating log folder: {folder}")
+                    os.makedirs(folder)
+            except Exception as e:
+                print(f"Failed to create directory {folder}: {e}", file=sys.stderr)
+                raise
 
     @staticmethod
     def _setup_logger(name, log_file, level=logging.INFO):
         """To setup as many loggers as you want"""
-        handler = logging.FileHandler(log_file)
-        formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
-        handler.setFormatter(formatter)
+        try:
+            handler = logging.FileHandler(log_file)
+            formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+            handler.setFormatter(formatter)
 
-        logger = logging.getLogger(name)
-        logger.setLevel(level)
-        logger.addHandler(handler)
-        if level == logging.INFO:
-            logger.addHandler(logging.StreamHandler(sys.stdout))
+            logger = logging.getLogger(name)
+            logger.setLevel(level)
+            logger.addHandler(handler)
+            if level == logging.INFO:
+                logger.addHandler(logging.StreamHandler(sys.stdout))
 
-        logger.debug("logging started")
+            logger.debug("logging started")
 
-        return logger
+            return logger
+        except Exception as e:
+            print(f"Failed to setup logger {name}: {e}", file=sys.stderr)
+            raise
 
     def debug(self, msg):
         self.logger.debug(msg)
